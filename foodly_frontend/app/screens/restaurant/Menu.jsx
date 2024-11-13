050bde89103afb095cf80d83bdadb48824d6c41c
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2024-11-13 18:27:08 +0700

    feat: food tile

--- foodly_frontend/app/screens/restaurant/Menu.jsx
+++ foodly_frontend/app/screens/restaurant/Menu.jsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { RestaurantContext } from "../../context/RestaurantContext";
+import uidata from "../../constants/uidata";
+import FoodTile from "../../components/FoodTile";
 
 const Menu = () => {
+  const navigation = useNavigation();
+  const { restaurantObj, setRestaurantObj } = useContext(RestaurantContext);
+
   return (
-    <View>
-      <Text>Menu</Text>
+    <View style={{ marginTop: 5 }}>
+      <FlatList
+        data={uidata.foods}
+        showsVerticalScrollIndicator={false}
+        style={{ marginTop: 5 }}
+        keyExtractor={(item) => item._id}
+        scrollEnabled
+        renderItem={({ item }) => (
+          <FoodTile
+            item={item}
+            showDetails={() => navigation.navigate("food-nav", item)}
+          />
+        )}
+      />
     </View>
   );
 };
--- foodly_frontend/app/screens/restaurant/New.jsx
+++ foodly_frontend/app/screens/restaurant/New.jsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { RestaurantContext } from "../../context/RestaurantContext";
+import FoodTile from "../../components/FoodTile";
+import uidata from "../../constants/uidata";
 
 const New = () => {
+  const navigation = useNavigation();
+  const { restaurantObj, setRestaurantObj } = useContext(RestaurantContext);
+
   return (
-    <View>
-      <Text>New</Text>
+    <View style={{ marginTop: 5 }}>
+      <FlatList
+        data={uidata.foods}
+        showsVerticalScrollIndicator={false}
+        style={{ marginTop: 5 }}
+        keyExtractor={(item) => item._id}
+        scrollEnabled
+        renderItem={({ item }) => (
+          <FoodTile
+            item={item}
+            showDetails={() => navigation.navigate("food-nav", item)}
+          />
+        )}
+      />
     </View>
   );
 };
